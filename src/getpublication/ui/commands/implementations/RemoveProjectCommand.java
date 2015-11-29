848c622cbb38ab650d77f42481570d9e3112b291
jcarlos <carlosmaster10@gmail.com>
2015-11-29 00:09:50 -0300

    RemoveProjectCommand updated

--- src/getpublication/ui/commands/implementations/RemoveProjectCommand.java
+++ src/getpublication/ui/commands/implementations/RemoveProjectCommand.java
@@ -12,22 +12,23 @@ public class RemoveProjectCommand implements Command {
     public void action(ContextCommand context) {
         JsonPublication jsonPublication = context.getJsonPublication();
         ((JsonBasicOperations) jsonPublication).load();
-        
+
         StringChooser stringChooser = new StringChooser();
         stringChooser.setTitle("type a number of one project:");
         for (String projectName : jsonPublication.getProjects()) {
             stringChooser.addString(projectName);
         }
-        
+
         String selectedProject = stringChooser.runWithIndex();
         if (selectedProject != null && !selectedProject.equals("")) {
             jsonPublication.removeProject(selectedProject);
-            
+
             ((JsonBasicOperations) jsonPublication).save();
-            if (context.getProject().getName().equals(selectedProject)) {
+            if (context.getProject() != null
+                    && context.getProject().getName().equals(selectedProject)) {
                 context.setProject(null);
             }
-            System.out.println("project "+selectedProject+" removed");
+            System.out.println("project " + selectedProject + " removed");
         } else {
             System.out.println("remove canceled");
         }
