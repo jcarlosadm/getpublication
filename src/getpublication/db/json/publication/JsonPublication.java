34b4534ed8be5695bb9d345e61a12fab1ea532c5
jcarlos <carlosmaster10@gmail.com>
2015-12-03 01:35:08 -0300

    JsonPublication modified for abstract class

--- src/getpublication/db/json/publication/JsonPublication.java
+++ src/getpublication/db/json/publication/JsonPublication.java
@@ -2,18 +2,34 @@ package getpublication.db.json.publication;
 
 import java.util.Set;
 
-public interface JsonPublication {
+import org.json.simple.JSONObject;
+
+import getpublication.db.json.JsonBasicOperations;
+
+public abstract class JsonPublication extends JsonBasicOperations {
     
     /**
      * @param projectName project to add
      */
-    public void addProject(String projectName);
+    @SuppressWarnings("unchecked")
+    public void addProject(String projectName){
+        if (!this.hasProject(projectName)) {
+            this.getJsonObject().put(projectName, new JSONObject());
+        }
+    }
     
     /**
      * @param projectName project to remove
      * @return true if success
      */
-    public boolean removeProject(String projectName);
+    public boolean removeProject(String projectName){
+        if (!this.hasProject(projectName)) {
+            return false;
+        }
+
+        this.getJsonObject().remove(projectName);
+        return true;
+    }
     
     /**
      * add project property
@@ -21,8 +37,15 @@ public interface JsonPublication {
      * @param propertyName property name
      * @param propertyValue property value
      */
+    @SuppressWarnings("unchecked")
     public void addProjectProperty(String projectName, PropertiesName propertyName,
-            String propertyValue);
+            String propertyValue){
+        if (!this.hasProject(projectName)) {
+            return;
+        }
+        JSONObject obj = (JSONObject) this.getJsonObject().get(projectName);
+        obj.put(propertyName.toString(), propertyValue);
+    }
     
     /**
      * remove project property
@@ -30,7 +53,15 @@ public interface JsonPublication {
      * @param propertyName property name
      * @return true if success
      */
-    public boolean removeProjectProperty(String projectName, PropertiesName propertyName);
+    public boolean removeProjectProperty(String projectName, PropertiesName propertyName){
+        if (!this.hasProjectProperty(projectName, propertyName)) {
+            return false;
+        }
+
+        JSONObject obj = (JSONObject) this.getJsonObject().get(projectName);
+        obj.remove(propertyName.toString());
+        return true;
+    }
     
     /**
      * get project property
@@ -38,14 +69,25 @@ public interface JsonPublication {
      * @param propertyName property name
      * @return value of project property
      */
-    public String getProjectProperty(String projectName, PropertiesName propertyName);
+    public String getProjectProperty(String projectName, PropertiesName propertyName){
+        if (!this.hasProjectProperty(projectName, propertyName)) {
+            return "";
+        }
+        JSONObject obj = (JSONObject) this.getJsonObject().get(projectName);
+        return ((String) obj.get(propertyName.toString()));
+    }
     
     /**
      * Check if project exists
      * @param project project name
      * @return true if this project exists
      */
-    public boolean hasProject(String project);
+    public boolean hasProject(String project){
+        if (!this.getJsonObject().containsKey(project)) {
+            return false;
+        }
+        return true;
+    }
     
     /**
      * Check if project property exists
@@ -53,10 +95,25 @@ public interface JsonPublication {
      * @param property property name
      * @return true if project property exists
      */
-    public boolean hasProjectProperty(String project, PropertiesName property);
+    public boolean hasProjectProperty(String project, PropertiesName property){
+        if (!this.getJsonObject().containsKey(project)) {
+            return false;
+        }
+
+        JSONObject obj = (JSONObject) this.getJsonObject().get(project);
+
+        if (!obj.containsKey(property.toString())) {
+            return false;
+        }
+
+        return true;
+    }
     
     /**
      * @return set of all projects
      */
-    public Set<String> getProjects();
+    @SuppressWarnings("unchecked")
+    public Set<String> getProjects(){
+        return this.getJsonObject().keySet();
+    }
 }
